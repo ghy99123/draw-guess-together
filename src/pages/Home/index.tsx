297343f9258b85afd4e008ddfc0b611403b1df41
ghy <ghy@ghydeMacBook-Air.local>
2022-07-05 19:45:02 +0800

    feat: add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate("/room");
+  };
+  return (
+    <>
+      <div className="container">
+        <form className="login-form">
+          <h2>欢迎登录</h2>
+          <input type="text" placeholder="请随意输入玩家名称" />
+          <button type="submit" onClick={onClick}>
+            加入
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
